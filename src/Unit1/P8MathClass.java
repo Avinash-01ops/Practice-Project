294199c32a24acdf07d949f1921daadd7cde7d6a
Avinash-01ops <avinashoza0101@gmail.com>
2024-09-03 16:53:43 +0530

    Stringoperators

---
+++ src/Unit1/P8MathClass.java
@@ -0,0 +1,17 @@
+package Unit1;
+
+public class P8MathClass {
+    //This program is created for Math Class
+    public static void main(String[] args) {
+    int x = 10;
+    int y = 5;
+    int z = 64;
+    int a = -100;
+
+        System.out.println("Maximum value of x and y is: "+Math.max(x,y)); //Max value
+        System.out.println("Minimum value of x and y is: "+Math.min(x,y)); //Min value
+        System.out.println("Square root of z is: "+Math.sqrt(z)); //Square root
+        System.out.println("Absolute value of a is: "+Math.abs(a)); //Absolute value
+        System.out.println("Random value between 0 and 1 is: "+Math.random()*101); //Random value
+    }
+}
