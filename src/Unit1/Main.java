f089982e2172a3a81388be4f86a748a5cfcaf54a
Avinash-01ops <avinashoza0101@gmail.com>
2024-09-03 12:02:12 +0530

    Updated

--- src/Unit1/Main.java
+++ src/Unit1/Main.java
@@ -1,7 +1,16 @@
 package Unit1;
+import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(5000);
     }
+    //Comments---------------------------------------------------------
+    //This is a single line comment
+    /*
+    This is a multi-line comment
+    */
+    //---------------------------------------------------------
+
 }
--- src/Unit1/P2Variables.java
+++ src/Unit1/P2Variables.java
@@ -1,2 +1,29 @@
-package Unit1;public class P2Variables {
+package Unit1;
+public class P2Variables {
+    //This Code is for Variables
+    /*
+    String - stores text, such as "Hello". String values are surrounded by double quotes
+    int - stores integers (whole numbers), without decimals, such as 123 or -123
+    float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+    char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+    boolean - stores values with two states: true or false
+     */
+    public static void main(String[] args) {
+        int a=10; //integer
+        String b="hello"; //String
+        float c=10.5f; //float
+        char d='a'; //char
+        boolean e=true; //boolean
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
 }
